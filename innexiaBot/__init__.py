b064ad6a6ec8c666942be41f6b73a16ce06324bf
Dark Cyber <87921701+DarkCybers@users.noreply.github.com>
2021-07-31 23:29:03 +0530

    Update __init__.py

--- innexiaBot/__init__.py
+++ innexiaBot/__init__.py
@@ -189,8 +189,8 @@ else:
 
 
 updater = tg.Updater(TOKEN, workers=WORKERS, use_context=True)
-telethn = TelegramClient("layla", API_ID, API_HASH)
-pbot = Client("laylapbot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
+telethn = TelegramClient("innexia", API_ID, API_HASH)
+pbot = Client("InnexiaBot", api_id=API_ID, api_hash=API_HASH, bot_token=TOKEN)
 dispatcher = updater.dispatcher
 
 DRAGONS = list(DRAGONS) + list(DEV_USERS)
