22cb18fdc997fb93a70ddecf268256683b41f794
Dark Cyber <87921701+DarkCybers@users.noreply.github.com>
2021-08-01 02:24:15 +0530

    Create voice.py

---
+++ innexiaBot/modules/voice.py
@@ -0,0 +1,13 @@
+__mod_name__ = "VcPlayer"
+
+__help__ = """
+**Commands** 🛠
+/play <song name> - play song you requested
+/song <song name> - download songs you want quickly
+
+**👮 Admins only.**
+
+/pause - pause song play
+/resume - resume song play
+/skip - play next song
+/end - stop music play
