f1f3c91170230be93da5f95a47ffc5eca8fbddc0
Dark Cyber <87921701+DarkCybers@users.noreply.github.com>
2021-07-31 23:38:00 +0530

    Update misc.py

--- innexiaBot/modules/helper_funcs/misc.py
+++ innexiaBot/modules/helper_funcs/misc.py
@@ -67,11 +67,11 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if len(pairs) > 8:
         pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
             (EqInlineKeyboardButton("⬅️", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("Back", callback_data="layla_back"),
+                EqInlineKeyboardButton("Back", callback_data="innexia_back"),
              EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("Back", callback_data="layla_back")]]
+        pairs += [[EqInlineKeyboardButton("Back", callback_data="innexia_back")]]
 
     return pairs
 
