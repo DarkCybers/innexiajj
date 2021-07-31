6297fb2b5f7540b995db94621742b8843aab97a8
Dark Cyber <87921701+DarkCybers@users.noreply.github.com>
2021-07-31 23:30:20 +0530

    Update __forcesubs.py

--- innexiaBot/modules/__forcesubs.py
+++ innexiaBot/modules/__forcesubs.py
@@ -181,7 +181,7 @@ def config(client, message):
 
 __help__ = """
 *Force Subscribe:*
-❍ Layla can mute members who are not subscribed your channel until they subscribe
+❍ Innexia can mute members who are not subscribed your channel until they subscribe
 ❍ When enabled I will mute unsubscribed members and show them a unmute button. When they pressed the button I will unmute them
 *Setup*
 *Only creator*
