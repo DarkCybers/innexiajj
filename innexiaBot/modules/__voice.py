76353115534422445717d76a4e60257f1d7c5e07
Dark Cyber <87921701+DarkCybers@users.noreply.github.com>
2021-08-01 12:19:03 +0530

    Update __voice.py

--- innexiaBot/modules/__voice.py
+++ innexiaBot/modules/__voice.py
@@ -2,12 +2,12 @@ __mod_name__ = "VcPlayer"
 
 __help__ = """
 **Commands** 🛠
-/play <song name> - play song you requested
-/song <song name> - download songs you want quickly
+• /play <song name> - play song you requested
+• /song <song name> - download songs you want quickly
 
 **👮 Admins only.**
 
-/pause - pause song play
-/resume - resume song play
-/skip - play next song
-/end - stop music play
+• /pause - pause song play
+• /resume - resume song play
+• /skip - play next song
+• /end - stop music play
