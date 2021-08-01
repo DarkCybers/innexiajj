12e9b8f1a4cbaee8706a75a8a246c457199e62c6
Dark Cyber <87921701+DarkCybers@users.noreply.github.com>
2021-08-01 13:22:43 +0530

    Update __telegraph.py

--- innexiaBot/modules/__telegraph.py
+++ innexiaBot/modules/__telegraph.py
@@ -8,7 +8,7 @@ from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
 Sammy = "Innexia"
 telegraph = Telegraph()
-r = telegraph.create_account(short_name=Hero)
+r = telegraph.create_account(short_name=Sammy)
 auth_url = r["auth_url"]
 
 
